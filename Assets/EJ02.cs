715e584566e625ed44a2a2d3ea5284f1b75374cf
ambarpalermo <ambarpalermo06@gmail.com>
2022-04-08 08:34:17 -0300

    todo terminado

--- Assets/EJ02.cs
+++ Assets/EJ02.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class EJ02 : MonoBehaviour
 {
+    public int precio1;
+    public int precio2;
+    public int precio3;
+
+    public int dinero;
+
+    int precioT;
     // Start is called before the first frame update
     void Start()
     {
-        
+        precioT = precio1 + precio2 + precio3;
+
+        if (precioT > dinero)
+        {
+            int falta = precioT - dinero; 
+            Debug.Log("No alcanza el dinero, faltan " + falta + " pesos");
+        }
+        else if (precioT < dinero)
+        {
+            int sobra = dinero - precioT;
+            Debug.Log("Alcanza y sobran " + sobra + "pesos");
+        }
     }
 
     // Update is called once per frame
